800d61f32443340e24f633895406a1fcaae5bb6e
Vincent Fumo <vincent_fumo@cable.comcast.com>
2014-03-26 17:02:55 -0400

    adds a test

---
+++ src/test/java/com/neodem/coup/common/CoupGameContextTest.java
@@ -0,0 +1,28 @@
+package com.neodem.coup.common;
+
+import com.neodem.coup.client.DoNothingCoupPlayer;
+import org.apache.commons.lang.SerializationUtils;
+import org.junit.Test;
+
+import java.io.Serializable;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+/**
+ * Author: Vincent Fumo (vfumo) : vincent_fumo@cable.comcast.com
+ * Created Date: 3/26/14
+ */
+public class CoupGameContextTest {
+
+    @Test
+    public void coupGameContextShouldSerializeCorrectly() {
+        CoupPlayer p = new DoNothingCoupPlayer();
+        CoupGameContext gc = new CoupGameContext();
+        gc.addPlayer(p);
+
+        Serializable original = gc;
+        Serializable copy = (Serializable) SerializationUtils.clone(original);
+        assertThat(original, equalTo(copy));
+    }
+}
