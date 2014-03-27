d5c7e31755304842adf8db35a84e29356aed8304
Vincent Fumo <vincent_fumo@cable.comcast.com>
2014-03-27 11:48:33 -0400

    adds test for RCP

--- coupClient/src/main/java/com/neodem/coup/client/RandomCoupPlayer.java
+++ coupClient/src/main/java/com/neodem/coup/client/RandomCoupPlayer.java
@@ -34,6 +34,23 @@ public class RandomCoupPlayer implements CoupPlayer, Serializable {
     private CoupPlayerInfo myState = null;
     private Random r = new Random(System.currentTimeMillis());
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof RandomCoupPlayer)) return false;
+
+        RandomCoupPlayer that = (RandomCoupPlayer) o;
+
+        if (myName != null ? !myName.equals(that.myName) : that.myName != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        return myName != null ? myName.hashCode() : 0;
+    }
+
     public RandomCoupPlayer(String name) {
         myName = name;
     }
